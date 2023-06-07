36cd64aa4765ebf0747bcb8b12bfc888714d94a9
VladimirovMario <mariovladimirovfs@gmail.com>
2023-06-07 18:45:02 +0300

    banner for new arrivals to do: extend functionality

---
+++ client/src/components/Home/Slider/NewArrivals/NewArrivals.js
@@ -0,0 +1,13 @@
+import styles from "./NewArrivals.module.css";
+
+export default function NewArrivals() {
+  return (
+    <div className={styles["image-wrapper"]}>
+      <img
+        className={styles["arrivals-image"]}
+        src="/static/new-arrivals/new-arrivals.jpg"
+        alt="new-arrivals.jpg"
+      />
+    </div>
+  );
+}
