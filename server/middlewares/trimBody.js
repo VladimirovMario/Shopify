f184689b1aa78416496601ee89c18369abec3343
VladimirovMario <mariovladimirovfs@gmail.com>
2023-08-11 17:39:33 +0300

    Enhance Request Body Trimming and Nested Object Trimming
    Building upon the existing functionality, this commit introduces an enhancement to the request body processing middleware.
    The middleware now includes logic to trim leading and trailing whitespace from string values within the request body.
    Additionally, for nested objects, the middleware iterates through each property to perform the same whitespace trimming on string values.
    
    This improvement ensures consistent and sanitized input data, promoting data integrity throughout the application.

--- server/middlewares/trimBody.js
+++ server/middlewares/trimBody.js
@@ -1,9 +1,28 @@
+const { checkType } = require('../helpers/checkType');
 module.exports = () => (req, res, next) => {
-    for (let key in req.body) {
-        if (typeof req.body[key] == 'string') {
-            req.body[key] = req.body[key].trim();
-        }
+  /*
+  The outer loop iterates over each key in the req.body object.
+  Inside the loop, if the value associated with the key is a string,
+  leading and trailing whitespace are removed using the trim() method.
+  */
+  for (let key in req.body) {
+    if (typeof req.body[key] == 'string') {
+      req.body[key] = req.body[key].trim();
     }
 
-    next();
+    /*
+    If the value is determined to be an object by calling the checkType
+    function, an inner loop is used to iterate over each property in the object.
+    Within the inner loop, if a property value is a string, the leading and trailing whitespace are removed.
+    */
+    if (checkType(req.body[key]) === 'object') {
+      for (const item in req.body[key]) {
+        if (typeof req.body[key][item] == 'string') {
+          req.body[key][item] = req.body[key][item].trim();
+        }
+      }
+    }
+  }
+
+  next();
 };
