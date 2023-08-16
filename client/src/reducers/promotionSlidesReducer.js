9958718fd83238192222c6962f8b91afde7e34e7
VladimirovMario <mariovladimirovfs@gmail.com>
2023-08-16 18:10:14 +0300

    Enhance Reducer Functionality: Handle Create, Edit, and Delete Operations
    In this commit, I've expanded the reducer by adding cases to handle create, edit, and delete operations within the PromotionSlidesContext. These additions bolster the context's ability to manage slide data and interactions effectively, providing a comprehensive solution for slide management.

--- client/src/reducers/promotionSlidesReducer.js
+++ client/src/reducers/promotionSlidesReducer.js
@@ -3,7 +3,19 @@ export default function promotionSlidesReducer(slides, action) {
     case 'INITIAL_SLIDES': {
       return [...action.slides];
     }
-
+    case 'create': {
+      const { title, description, imageUrl } = action.slide;
+      const id = action.id;
+      return [{ id, title, description, imageUrl, isActive: true }, ...slides];
+    }
+    case 'edit': {
+      return slides.map((slide) =>
+        slide._id === action.slide._id ? action.slide : slide
+      );
+    }
+    case 'delete': {
+      return slides.filter((slide) => slide._id !== action.slide._id);
+    }
     default:
       throw Error(`Unknown action: ${action.type}`);
   }
