8223d95438fb38fab65f0a872bbdacd27cb7f448
VladimirovMario <mariovladimirovfs@gmail.com>
2023-08-18 18:17:12 +0300

    Service Functionality Expansion: Add POST, PUT, and DELETE Requests in promoSlidesService
    This commit introduces three new service functions within promoSlidesService to facilitate communication with the server. The added functions handle POST, PUT, and DELETE requests, enabling seamless data interaction between the frontend and the backend for managing promotional slides.

--- client/src/services/promoSliderService.js
+++ client/src/services/promoSliderService.js
@@ -1,11 +1,26 @@
 import { requestFactory } from './requester';
 
 const endpoints = {
-  allSlides: '/api/promotion-slide/',
+  allSlides: '/api/promotion-slide',
+  createSlide: '/api/promotion-slide',
+  editSlide: '/api/promotion-slide/',
+  deleteSlide: '/api/promotion-slide/',
 };
 
 const request = requestFactory();
 
 export const getAllPromoSlides = async () => {
   return await request.get(endpoints.allSlides);
+};
+
+export const cretePromoSlide = async (data) => {
+  return await request.post(endpoints.createSlide, data);
+};
+
+export const editPromoSlide = async (id, data) => {
+  return await request.put(endpoints.editSlide + id, data);
+};
+
+export const deletePromoSlide = async (id) => {
+  return await request.delete(endpoints.deleteSlide + id);
 };
