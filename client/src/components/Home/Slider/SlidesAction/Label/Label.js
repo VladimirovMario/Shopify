67a93c1f0060bc313a19a8a6a0353fddcb91f3f5
VladimirovMario <mariovladimirovfs@gmail.com>
2023-08-20 19:03:28 +0300

    Added Span to show if there is an input error

--- client/src/components/Home/Slider/SlidesAction/Label/Label.js
+++ client/src/components/Home/Slider/SlidesAction/Label/Label.js
@@ -1,11 +1,21 @@
-export default function Label({ children, className, inputId, spanText }) {
+import styles from './Label.module.css';
+
+export default function Label({
+  children,
+  className,
+  inputId,
+  spanText,
+  error,
+}) {
   return (
     <label className={className} htmlFor={inputId}>
       <div>
         <span>
           {spanText}
           {/* In case of error */}
-          {/* &nbsp;<span className={styles.error}>Error message</span> */}
+          {error && (
+            <span className={styles['error-message']}>&nbsp;{error}</span>
+          )}
         </span>
       </div>
       {children}
