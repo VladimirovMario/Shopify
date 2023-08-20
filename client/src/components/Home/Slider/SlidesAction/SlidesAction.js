20081b8428d63f7d0f5df05d133127cc12271723
VladimirovMario <mariovladimirovfs@gmail.com>
2023-08-20 19:15:14 +0300

    Refactor: Migrate to useForm Hook for Form Handling
    In this commit, I've refactored the code to utilize the custom useForm hook for form handling. This hook provides an organized approach to managing form state, validation, and errors. The values, errors, and relevant handlers such as onChangeHandler, onValidateForm, and onCheckboxHandler are now obtained from the hook. The onSubmit function has been updated to rely on the values object for form data, and it also performs a check for non-empty string fields before proceeding with the selected action. These changes enhance code modularity and maintainability while ensuring a streamlined and error-resistant form interaction process.

--- client/src/components/Home/Slider/SlidesAction/SlidesAction.js
+++ client/src/components/Home/Slider/SlidesAction/SlidesAction.js
@@ -1,9 +1,10 @@
-import { useId, useState } from 'react';
+import { useId } from 'react';
 import { useSlidesContext } from '../../../../contexts/PromotionSlidesContext';
 import BackButton from '../../../Shared/Buttons/BackButton/BackButton';
 import styles from '../Slider.module.css';
 import Label from './Label/Label';
 import Input from './Input/Input';
+import { useForm } from '../../../../hooks/useForm';
 
 export default function SlidesAction({ selectedAction }) {
   const {
@@ -16,31 +17,41 @@ export default function SlidesAction({ selectedAction }) {
   } = useSlidesContext();
 
   const id = useId();
-  const [formValues, setFormValues] = useState({
-    _id: wantedSlide._id,
-    title: selectedAction === 'create' ? '' : wantedSlide.title,
-    description: selectedAction === 'create' ? '' : wantedSlide.description,
-    imageUrl: selectedAction === 'create' ? '' : wantedSlide.imageUrl,
-    isActive: selectedAction === 'create' ? true : wantedSlide.isActive,
-  });
+
+  const { values, errors, onChangeHandler, onValidateForm, onCheckboxHandler } =
+    useForm({
+      _id: selectedAction === 'create' ? '' : wantedSlide._id,
+      title: selectedAction === 'create' ? '' : wantedSlide.title,
+      description: selectedAction === 'create' ? '' : wantedSlide.description,
+      imageUrl: selectedAction === 'create' ? '' : wantedSlide.imageUrl,
+      isActive: selectedAction === 'create' ? true : wantedSlide.isActive,
+    });
 
   async function onSubmit(e) {
     e.preventDefault();
     const slide = {};
 
-    if (selectedAction === 'create') {
-      const createdSlide = await onCreateSubmit(formValues);
-      Object.assign(slide, createdSlide);
-    }
+    const nonEmptyStringFields = Object.values(values).every((v) =>
+      typeof v === 'string' ? v.trim() !== '' : v
+    );
 
-    if (selectedAction === 'edit') {
-      const editedSlide = await onEditSubmit(formValues);
-      Object.assign(slide, editedSlide);
-    }
+    if (nonEmptyStringFields) {
+      if (selectedAction === 'create') {
+        const createdSlide = await onCreateSubmit(values);
+        Object.assign(slide, createdSlide);
+      }
 
-    if (selectedAction === 'delete') {
-      const deletedSlide = await onDeleteSubmit(formValues._id);
-      Object.assign(slide, deletedSlide);
+      if (selectedAction === 'edit') {
+        const editedSlide = await onEditSubmit(values);
+        Object.assign(slide, editedSlide);
+      }
+
+      if (selectedAction === 'delete') {
+        const deletedSlide = await onDeleteSubmit(values._id);
+        Object.assign(slide, deletedSlide);
+      }
+    } else {
+      alert('All fields are required!');
     }
 
     dispatch({
@@ -49,14 +60,6 @@ export default function SlidesAction({ selectedAction }) {
     });
 
     resetSelectedSlide();
-  }
-
-  function onChange(e) {
-    setFormValues((state) => ({ ...state, [e.target.name]: e.target.value }));
-  }
-
-  function handleIsActive(e) {
-    setFormValues((state) => ({ ...state, [e.target.name]: e.target.checked }));
   }
 
   function handleBackClick() {
@@ -77,15 +80,17 @@ export default function SlidesAction({ selectedAction }) {
             className={styles['input-label']}
             inputId={`${id}-title`}
             spanText="Title"
+            error={errors.title}
           >
             <Input
               type="text"
               id={`${id}-title`}
               name="title"
-              value={formValues.title}
-              onChange={onChange}
+              value={values.title}
+              onChange={onChangeHandler}
               placeholder="Unleash Gaming Potential"
               disabled={isDisabled}
+              onBlur={onValidateForm}
             />
           </Label>
           {/* Image url input */}
@@ -93,15 +98,17 @@ export default function SlidesAction({ selectedAction }) {
             className={styles['input-label']}
             inputId={`${id}-imageUrl`}
             spanText="Image url"
+            error={errors.imageUrl}
           >
             <Input
               type="text"
               id={`${id}-imageUrl`}
               name="imageUrl"
-              value={formValues.imageUrl}
-              onChange={onChange}
+              value={values.imageUrl}
+              onChange={onChangeHandler}
               placeholder="https://"
               disabled={isDisabled}
+              onBlur={onValidateForm}
             />
           </Label>
 
@@ -109,17 +116,19 @@ export default function SlidesAction({ selectedAction }) {
             className={styles['input-label']}
             inputId={`${id}-description`}
             spanText="Description"
+            error={errors.description}
           >
             <textarea
               className={styles.description}
               id={`${id}-description`}
               name="description"
-              value={formValues.description}
-              onChange={onChange}
+              value={values.description}
+              onChange={onChangeHandler}
               placeholder="Find Perfect Match with PS4 and PS5 Games"
               cols="30"
               rows="3"
               disabled={isDisabled}
+              onBlur={onValidateForm}
             />
           </Label>
           <input
@@ -141,8 +150,8 @@ export default function SlidesAction({ selectedAction }) {
                 id={`${id}-isActive`}
                 name="isActive"
                 type="checkbox"
-                checked={formValues.isActive}
-                onChange={handleIsActive}
+                checked={values.isActive}
+                onChange={onCheckboxHandler}
                 disabled={isDisabled}
               />
             </Label>
