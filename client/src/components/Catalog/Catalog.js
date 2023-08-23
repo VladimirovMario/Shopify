52900b7e790c8f562a0837f18ef1a94567df501f
VladimirovMario <mariovladimirovfs@gmail.com>
2023-08-23 16:24:41 +0300

    Removed SEARCH FORM from Catalog

--- client/src/components/Catalog/Catalog.js
+++ client/src/components/Catalog/Catalog.js
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-
 import { useGameContext } from '../../contexts/GameContext';
 import { Loader } from '../Shared/Loader/Loader';
 import ProductPs4 from '../ProductPs4/ProductPs4';
@@ -8,28 +5,7 @@ import ProductPs4 from '../ProductPs4/ProductPs4';
 import styles from './Catalog.module.css';
 
 export default function Catalog() {
-  const { games, loading, searchGamesHandler } = useGameContext();
-  let [searchParams, setSearchParams] = useSearchParams();
-
-  const [value, setValues] = useState({
-    search: '',
-  });
-
-  const onChangeHandler = (e) => {
-    setValues((state) => ({ ...state, [e.target.name]: e.target.value }));
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-
-    if (value.search) {
-      setSearchParams(value);
-    } else {
-      setSearchParams('');
-    }
-    // TODO the value of (searchParams) updates at the second click
-    searchGamesHandler(value.search);
-  };
+  const { games, loading } = useGameContext();
 
   return (
     <section className="section">
@@ -40,32 +16,13 @@ export default function Catalog() {
 
       {/* <!-- Products --> */}
       <div className={styles['product-wrapper']}>
-        {/* <!-- SEARCH FORM --> */}
-        <form onSubmit={onSubmit} className={styles['search-form']}>
-          <label className={styles['search-label']} htmlFor="search">
-            <span className={styles['search-terms']}>Title:</span>
-            <input
-              className={styles['search-input']}
-              id="search"
-              type="search"
-              autoComplete="off"
-              placeholder="Start searching for..."
-              name="search"
-              value={value.search}
-              onChange={onChangeHandler}
-            />
-          </label>
-
-          <button className={'action-btn edit-btn btn'}>Search</button>
-        </form>
-
+        {/* Loading component*/}
         {loading && <Loader />}
 
         <ul className={styles['product-ul']}>
           {!loading &&
             games.map((game) => <ProductPs4 key={game._id} {...game} />)}
         </ul>
-
         {!games.length && (
           <h3 className={styles['no-publications']}>
             No publications yet. Be the first one.
